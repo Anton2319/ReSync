b8f9bb324a996990c0533176bd630e6e541046b8
Antony Rudkovsky <anton.rudkovskij1993@gmail.com>
2022-04-08 21:50:25 +0300

    restriction by chat

--- handlers/yes/yes.py
+++ handlers/yes/yes.py
@@ -5,7 +5,7 @@ deps = 'None'
 description = 'Пизда'
 
 yestrue = True if re.findall(r"\b[дДdD][AАаа]+[^\wБ-Яб-яёЁ]*$\Z", text) else False
-if yestrue:
+if yestrue and peer_id == -1001251682478:
     print("yestruecorrect")
     message("Пизда")
 #restricted
