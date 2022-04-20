1a738046d7cd40ea3b0c04c197a0e205df0269c8
Antony Rudkovsky <anton.rudkovskij1993@gmail.com>
2022-04-20 18:39:24 +0300

    Added title

--- commands/sync/sync.py
+++ commands/sync/sync.py
@@ -17,19 +17,19 @@ def pageProcess(pageToken):
     global playlistID
     global uploaded_content
     if pageToken is None:
-        payload = {"key": youtubeDataApi_token, "maxResults": "5", "playlistId": playlistID, "part": "contentDetails"}
+        payload = {"key": youtubeDataApi_token, "maxResults": "5", "playlistId": playlistID, "part": "contentDetails, snippet"}
     else:
-        payload = {"key": youtubeDataApi_token, "maxResults": "5", "playlistId": playlistID, "part": "contentDetails",
+        payload = {"key": youtubeDataApi_token, "maxResults": "5", "playlistId": playlistID, "part": "contentDetails, snippet",
                    "pageToken": pageToken}
     headers = {'content-type': 'application/json'}
     r = requests.get('https://youtube.googleapis.com/youtube/v3/playlistItems', params=payload, headers=headers)
     json_data = r.json()
     for video in json_data["items"]:
         if 'https://youtube.com/watch?v='+video["contentDetails"]["videoId"] not in uploaded_content:
-            os.system('yt-dlp -f bestvideo+bestaudio --merge-output-format mp4 https://youtube.com/watch?v=' +
+            os.system('yt-dlp -f \'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best\' --merge-output-format mp4 https://youtube.com/watch?v=' +
                       video["contentDetails"]["videoId"] + ' --output download.mp4')
-            os.system('telegram-send --video download.mp4 --caption https://youtube.com/watch?v=' + video["contentDetails"][
-                "videoId"])
+            os.system('telegram-send --video download.mp4 --caption "'+ video["snippet"]["title"] + '\n' + video["snippet"]["videoOwnerChannelTitle"] + '\n' + 'https://youtube.com/watch?v=' + video["contentDetails"][
+                "videoId"]+'"')
             os.system('rm download.mp4')
             PlusWrite('https://youtube.com/watch?v='+video["contentDetails"]["videoId"]+'\n', "usr/syncronized.txt")
 
